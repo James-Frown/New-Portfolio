2e1296d18a54a20be792e05085c6a0ee9e1effd1
James-Frown <21122@ctca.co.za>
2023-05-05 12:20:16 +0200

    added the template for the skills page & added the first elements for the hero component

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,9 +1,38 @@
 import './hero.css';
 
+import { Link } from 'react-router-dom';
+
 const Hero = () => {
     return (
         <>
-
+            <div className='Hero__Container'>
+                <div className='Image__Wrapper'>
+                    <img />
+                </div>
+                <div className='Text__Wrapper'>
+                    <p className='Text__1'>
+                        Welcome,
+                    </p>
+                    <p className='Text__2'>
+                        My name is <span className='Text__A'> James Brown </span>
+                    </p>
+                    <p className='Text__3'>
+                        I am a <span className='Text__B'> Designer-Developer </span>
+                    </p>
+                    <div className='Button__Wrapper'>
+                        <a className="Page__Link__Primary">
+                            <Link exact to="/projects" className="Page__Link__Primary">
+                                Learn More
+                            </Link>
+                        </a>
+                        <a className="Page__Link__Secondary">
+                            <Link exact to="/projects" className="Page__Link__Secondary">
+                                My CV
+                            </Link>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
