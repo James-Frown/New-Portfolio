6b90c6bebf2a05f1c5bc8ee8265d3913ade837c6
James-Frown <21122@ctca.co.za>
2023-05-15 21:35:25 +0200

    Latest Changes

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,32 +1,28 @@
 import Behance from "../../assets/footer/Behance.svg";
 import Github from "../../assets/footer/GitHub.svg";
-import Instagram from "../../assets/footer/Instagram.svg";
 import LinkedIn from "../../assets/footer/LinkedIn.svg";
-import Slack from "../../assets/footer/Slack.svg";
 
 import './footer.css';
 
 const Footer = () => {
     return (
         <>
-            <div className='intrests__wrapper'>
-                <div className='intrests__content'>
-                    <div className='content__icons__wrapper'>
-                        <div className='icon__wrapper'>
+            <div className='footer__wrapper'>
+                <div className='footer__content'>
+                    <div className='icon__wrapper'>
+                        <a href="https://www.behance.net/jamesbrown158">
                             <img src={Behance} className='icon__selector' />
-                        </div>
-                        <div className='icon__wrapper'>
+                        </a>
+                    </div>
+                    <div className='icon__wrapper'>
+                        <a href="https://github.com/James-Frown">
                             <img src={Github} className='icon__selector' />
-                        </div>
-                        <div className='icon__wrapper'>
-                            <img src={Instagram} className='icon__selector' />
-                        </div>
-                        <div className='icon__wrapper'>
+                        </a>
+                    </div>
+                    <div className='icon__wrapper'>
+                        <a href="https://www.linkedin.com/in/james-brown-308233171">
                             <img src={LinkedIn} className='icon__selector' />
-                        </div>
-                        <div className='icon__wrapper'>
-                            <img src={Slack} className='icon__selector' />
-                        </div>
+                        </a>
                     </div>
                 </div>
             </div>
