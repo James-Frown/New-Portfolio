483c632093ca7a38f327cd709c481c5404329817
James-Frown <21122@ctca.co.za>
2023-05-15 17:23:43 +0200

    Latest Changes

--- src/pages/aboutPage/interests.jsx
+++ src/pages/aboutPage/interests.jsx
@@ -1,40 +1,43 @@
-
+import Icon1 from "../../assets/interests/ICON1.svg";
+import Icon2 from "../../assets/interests/ICON2.svg";
+import Icon3 from "../../assets/interests/ICON3.svg";
+import Icon4 from "../../assets/interests/ICON4.svg";
 import './about.css';
 
 const Intrests = () => {
     return (
         <>
-            <div className='Intrests__wrapper'>
-                <div className='Intrests__content'>
-                    <div className='content__text'>
-                        <h1 className='content__icons__text'>
+            <div className='intrests__wrapper'>
+                <div className='intrests__content'>
+                    <div className='content__title__wrapper'>
+                        <h1 className='content__title'>
                             Some of my intrests are:
                         </h1>
                     </div>
-                    <div className='content__icons__wrapepr'>
-                        <div className='icons__wrapepr'>
-                            <img src='' className='icons__selector' />
-                            <h2 className='content__icons__text'>
+                    <div className='content__icons__wrapper'>
+                        <div className='icon__wrapper'>
+                            <img src={Icon1} className='icon__selector' />
+                            <h3 className='icon__text'>
                                 Responsive Design
-                            </h2>
+                            </h3>
                         </div>
-                        <div className='icons__wrapepr'>
-                            <img src='' className='icons__selector' />
-                            <h2 className='content__icons__text'>
+                        <div className='icon__wrapper'>
+                            <img src={Icon2} className='icon__selector' />
+                            <h3 className='icon__text'>
                                 Web-App Designer
-                            </h2>
+                            </h3>
                         </div>
-                        <div className='icons__wrapepr'>
-                            <img src='' className='icons__selector' />
-                            <h2 className='content__icons__text'>
+                        <div className='icon__wrapper'>
+                            <img src={Icon3} className='icon__selector' />
+                            <h3 className='icon__text'>
                                 Blockchain Technology
-                            </h2>
+                            </h3>
                         </div>
-                        <div className='icons__wrapepr'>
-                            <img src='' className='icons__selector' />
-                            <h2 className='content__icons__text'>
+                        <div className='icon__wrapper'>
+                            <img src={Icon4} className='icon__selector' />
+                            <h3 className='icon__text'>
                                 Web-App Developer
-                            </h2>
+                            </h3>
                         </div>
                     </div>
                 </div>
--- src/pages/aboutPage/summery.jsx
+++ src/pages/aboutPage/summery.jsx
@@ -1,30 +1,30 @@
 import ProfileImage from "../../assets/images/ProfileImage.jpg";
-import './about.css';
+import './summery.css';
 
 const Summery = () => {
     return (
         <>
             <div className='summery__wrapper'>
-                <div className='image__wrapper'>
-                    <img src={ProfileImage} className='image__Selector'/>
+                <div className='summery__image__wrapper'>
+                    <img src={ProfileImage} className='image__selector'/>
                 </div>
-                <div className='summery__content'>
+                <div className='summery__content__wrapper'>
                     <div className='content__text'>
-                        <p>
+                        <p className='text__selector'>
                             As a tech enthusiast with an entrepreneurial spirit, I am driven to innovate and create positive change. With my passion for learning and willingness to take calculated risks, I am constantly seeking opportunities to challenge myself and push beyond my limits. My hunger for knowledge fuels my creativity, resilience, and unwavering commitment to success.
                         </p>
-                        <p>
+                        <p className='text__selector'>
                             I am a self-starter and team player with excellent communication skills, and a strong drive to code. My diverse interests and experiences contribute to my ability to think outside the box and bring unique perspectives to my work. Furthermore, I am dedicated to leaving a lasting impact on the world through my work in the tech industry.
                         </p>
-                        <p>
+                        <p className='text__selector'>
                             In my spare time, I enjoy exploring new cultures, music, surfing, and fitness. These experiences have taught me to be adaptable and open-minded, which are qualities that I believe make me a valuable asset to any tech organization.
                         </p>
-                        <p>
+                        <p className='text__selector'>
                             I am currently seeking internship opportunities to further develop my skills and contribute to a tech organization. If given the opportunity, I am confident that I can bring my passion for innovation and ability to work well in a team to make meaningful contributions to the organization.
                         </p>
                     </div>
-                    <div>
-                        <a href="https://drive.google.com/file/d/1x-6Qrc-P0WaMp9GuWRJiViEYyHD2WAcn/view?usp=share_link" className="Page__Link__Secondary">
+                    <div className='cta__wrapper'>
+                        <a href="https://drive.google.com/file/d/1x-6Qrc-P0WaMp9GuWRJiViEYyHD2WAcn/view?usp=share_link" className="cta__button">
                             Resume
                         </a>
                     </div>
