cd22ac806b7a42ad9ac184f15351744571688cc6
James-Frown <21122@ctca.co.za>
2023-05-17 22:14:22 +0200

    Latest Changes

--- src/pages/skillsPage/codeSkills.jsx
+++ src/pages/skillsPage/codeSkills.jsx
@@ -1,6 +1,17 @@
 // import developer icons
-import CSS from '../../assets/skills/code/CSS3.svg';
-import HTML from '../../assets/skills/code/HTML5.svg';
+import CSS from '../../assets/skills/code/CSS3_Icon.svg';
+import HTML from '../../assets/skills/code/HTML5_Icon.svg';
+import JS from '../../assets/skills/code/JS_Icon.svg';
+import Java from '../../assets/skills/code/Java_Icon.svg';
+import Firebase from '../../assets/skills/code/Firebase_Icon.svg';
+import Git from '../../assets/skills/code/Git_Icon.svg';
+import MySql from '../../assets/skills/code/MySql_Icon.svg';
+import Netlify from '../../assets/skills/code/Netlify_Icon.svg';
+import React from '../../assets/skills/code/ReactNative_Icon.svg';
+import Saas from '../../assets/skills/code/Saas_Icon.svg';
+import StoryBook from '../../assets/skills/code/StoryBook_Icon.svg';
+import Tailwind from '../../assets/skills/code/TailwindCSS_Icon.svg';
+import TypeScript from '../../assets/skills/code/TypeScript_Icon.svg';
 
 // importing styling
 import './filterSkills.css';
@@ -12,10 +23,43 @@ const Code = () => {
                 Code...
             </h1>
             <div>
-                {CSS}
+                <img src={CSS} />
             </div>
             <div>
-                {HTML}
+                <img src={HTML} />
+            </div>
+            <div>
+                <img src={JS} />
+            </div>
+            <div>
+                <img src={Java} />
+            </div>
+            <div>
+                <img src={Firebase} />
+            </div>
+            <div>
+                <img src={Git} />
+            </div>
+            <div>
+                <img src={MySql} />
+            </div>
+            <div>
+                <img src={Netlify} />
+            </div>
+            <div>
+                <img src={React} />
+            </div>
+            <div>
+                <img src={Saas} />
+            </div>
+            <div>
+                <img src={StoryBook} />
+            </div>
+            <div>
+                <img src={Tailwind} />
+            </div>
+            <div>
+                <img src={TypeScript} />
             </div>
         </>
     );
