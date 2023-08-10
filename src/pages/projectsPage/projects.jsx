e4f62f8f80db5453c823b9d97f809a64e6c292b0
James-Frown <21122@ctca.co.za>
2023-08-10 11:53:36 +0200

    Little Update!

--- src/pages/projectsPage/projects.jsx
+++ src/pages/projectsPage/projects.jsx
@@ -15,147 +15,150 @@ import Nav from "../../components/navigation/nav";
 import Footer from "../../components/footer/footer";
 
 // Element Styling import
-import './projects.css';
+import "./projects.css";
 
 // importing project card
 import ProjectCard from "../../components/projects/ProjectCard";
 
 const Projects = () => {
-    const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        // set a timer to change isLoading state after 3 seconds
-        setTimeout(() => {
-            setIsLoading(false);
-        }, 3000);
-    });
+  useEffect(() => {
+    // set a timer to change isLoading state after 3 seconds
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  });
 
-    return (
+  const vercelLogo = "https://yt3.googleusercontent.com/ytc/AOPolaTWbjhVRtPrUtUPqrx3NFAP57KRFK9BDInW_GNlLw=s176-c-k-c0x00ffffff-no-rj-mo";
+
+  return (
+    <>
+      {isLoading ? (
         <>
-            {isLoading ? (
-                <>
-                    <div className="LoadingContainer">
-                        <div className="LoadingText">
-                            <motion.h1
-                                initial={{
-                                    x: "-100rem"
-                                }}
-                                animate={{
-                                    x: 0
-                                }}
-                                transition={{
-                                    duration: 1
-                                }}
-                            >
-                                Loading
-                            </motion.h1>
-                            <motion.h1
-                                initial={{
-                                    x: "100rem"
-                                }}
-                                animate={{
-                                    x: 0
-                                }}
-                                transition={{
-                                    duration: 1.3
-                                }}
-                            >
-                                .
-                            </motion.h1>
-                            <motion.h1
-                                initial={{
-                                    x: "100rem"
-                                }}
-                                animate={{
-                                    x: 0
-                                }}
-                                transition={{
-                                    duration: 1.6
-                                }}
-                            >
-                                .
-                            </motion.h1>
-                            <motion.h1
-                                initial={{
-                                    x: "100rem"
-                                }}
-                                animate={{
-                                    x: 0
-                                }}
-                                transition={{
-                                    duration: 1.9
-                                }}
-                            >
-                                .
-                            </motion.h1>
-                        </div>
-                    </div>
-                </>
-            ) : (
-                <>
-                    <Nav />
-                    <div>
-                        <h1>
-                            My Projects
-                        </h1>
-                    </div>
-                    <div>
-                        <h2>
-                            Basic Sites:
-                        </h2>
-                    </div>
-                    <div className="project_cards">
-                        <ProjectCard
-                            title="Tutorial Project"
-                            subtitle="NextJS Image Gallery"
-                            link="https://next-js-tutorial-1.vercel.app/"
-                            description="This was a project I undertook to try and learn more about NextJS, from a tutorial by Coding In Flow."
-                            thumbnail={NextJSTutorial}
-                        />
-                        <ProjectCard
-                            title="Tutorial Project"
-                            subtitle="Basic Netflix Clone"
-                            link="https://basic-streaming-clone.netlify.app/"
-                            description="This was a project I tried to cpoy the look and feel of soap2day, and Netflix inorder to make a fake streaming service."
-                            thumbnail={BasicClone}
-                        />
-                        <ProjectCard
-                            title="Tutorial Project"
-                            subtitle="Basic SaaS Site"
-                            link="https://basic-saas-website.netlify.app/"
-                            description="This was a project I tried to copy the look and feel of a SaaS site for a university project, however it was left unfinnished."
-                            thumbnail={BasicSaaS}
-                        />
-                    </div>
-                    <div>
-                        <h2>
-                            Digital Products:
-                        </h2>
-                    </div>
-                    <div className="project_cards">
-                        <ProjectCard
-                            title="Tutorial Project"
-                            subtitle="Basic Product Card"
-                            link="https://basic-product-card.netlify.app/"
-                            description="This was a project I tried to copy the look and feel of a Product card of private company's product."
-                            thumbnail={BasicProduct}
-                        />
-                        <ProjectCard
-                            title="Tutorial Project"
-                            subtitle="Basic NFT Card"
-                            link="https://basic-nft-card.netlify.app/"
-                            description="This was a project I undertook to try and copy the look and feel of NFT's."
-                            thumbnail={BasicNFT}
-                        />
-                    </div>
-                    <div className="image_wrapper" >
-                        <img src={Image} className="image_selector" />
-                    </div>
-                    <Footer />
-                </>
-            )}
+          <div className="LoadingContainer">
+            <div className="LoadingText">
+              <motion.h1
+                initial={{
+                  x: "-100rem",
+                }}
+                animate={{
+                  x: 0,
+                }}
+                transition={{
+                  duration: 1,
+                }}
+              >
+                Loading
+              </motion.h1>
+              <motion.h1
+                initial={{
+                  x: "100rem",
+                }}
+                animate={{
+                  x: 0,
+                }}
+                transition={{
+                  duration: 1.3,
+                }}
+              >
+                .
+              </motion.h1>
+              <motion.h1
+                initial={{
+                  x: "100rem",
+                }}
+                animate={{
+                  x: 0,
+                }}
+                transition={{
+                  duration: 1.6,
+                }}
+              >
+                .
+              </motion.h1>
+              <motion.h1
+                initial={{
+                  x: "100rem",
+                }}
+                animate={{
+                  x: 0,
+                }}
+                transition={{
+                  duration: 1.9,
+                }}
+              >
+                .
+              </motion.h1>
+            </div>
+          </div>
         </>
-    );
+      ) : (
+        <>
+          <Nav />
+          <div>
+            <h1>My Projects</h1>
+          </div>
+          <div>
+            <h2>Basic Sites:</h2>
+          </div>
+          <div className="project_cards">
+            <ProjectCard
+              title="Tutorial Project"
+              subtitle="NextJS Ecommerce Store"
+              link="https://next-store-james-frown.vercel.app/"
+              description="I Learnt about NextJS, API's, MongoDB, Next-Auth, Server Actions, Prisma and so much more! It is by Coding In Flow."
+              thumbnail={vercelLogo}
+            />
+            <ProjectCard
+              title="Tutorial Project"
+              subtitle="NextJS Image Gallery"
+              link="https://next-js-tutorial-1.vercel.app/"
+              description="This was a project I undertook to try and learn more about NextJS, from a tutorial by Coding In Flow."
+              thumbnail={NextJSTutorial}
+            />
+            <ProjectCard
+              title="Tutorial Project"
+              subtitle="Basic Netflix Clone"
+              link="https://basic-streaming-clone.netlify.app/"
+              description="This was a project I tried to cpoy the look and feel of soap2day, and Netflix inorder to make a fake streaming service."
+              thumbnail={BasicClone}
+            />
+            <ProjectCard
+              title="Tutorial Project"
+              subtitle="Basic SaaS Site"
+              link="https://basic-saas-website.netlify.app/"
+              description="This was a project I tried to copy the look and feel of a SaaS site for a university project, however it was left unfinnished."
+              thumbnail={BasicSaaS}
+            />
+          </div>
+          <div>
+            <h2>Digital Products:</h2>
+          </div>
+          <div className="project_cards">
+            <ProjectCard
+              title="Tutorial Project"
+              subtitle="Basic Product Card"
+              link="https://basic-product-card.netlify.app/"
+              description="This was a project I tried to copy the look and feel of a Product card of private company's product."
+              thumbnail={BasicProduct}
+            />
+            <ProjectCard
+              title="Tutorial Project"
+              subtitle="Basic NFT Card"
+              link="https://basic-nft-card.netlify.app/"
+              description="This was a project I undertook to try and copy the look and feel of NFT's."
+              thumbnail={BasicNFT}
+            />
+          </div>
+          <div className="image_wrapper">
+            <img src={Image} className="image_selector" />
+          </div>
+          <Footer />
+        </>
+      )}
+    </>
+  );
 };
 
 export default Projects;
